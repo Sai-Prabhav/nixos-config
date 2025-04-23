32f5dc023898ff9ea60964940d437214d15ddfe9
Sai Prabhav <saiprabhav.ss@gmail.com>
2025-04-23 17:32:55 +0530

    added home manager

--- flake.nix
+++ flake.nix
@@ -3,14 +3,19 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  home-manager.url = github:nix-community/home-manager;
-  };
+  home-manager={
+    url = github:nix-community/home-manager;
+    inputs.nixpkgs.follows="nixpkgs";
+	};
+ };
 
-  outputs = { self, nixpkgs,  ... }: {
+  outputs = { self, nixpkgs,  ... }@inputs: {
     nixosConfigurations.nixos= nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+     # extraSpecialArgs={inherit inputs;};
       modules = [
         ./configuration.nix
+        inputs.home-manager.nixosModules.default
       ];
       specialArgs = { inherit self; };
     };
