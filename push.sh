3087b9fce1edbb8e80e2c3cb2d90cb8e70125806
Sai-Prabhav <saiprabhav.ss@gmail.com>
2025-05-04 16:31:59 +0530

    Update 2025-05-04

---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    eval $(ssh-agent)
+    ssh-add ~/nixos-config/.ssh/id_ed25519       
+    echo "SSH agent started and key added."
+fi
+
+git add .
+git commit -m "Update $(date +%Y-%m-%d)"
+git push --set-upstream origin master       
